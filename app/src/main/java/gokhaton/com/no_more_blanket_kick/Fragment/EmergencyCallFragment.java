4a72a1ab41209d6427ce6b64c71cf288fb026be2
earl31 <hanulhalim31@korea.ac.kr>
2017-08-05 15:32:36 +0900

    commit - main activity

---
+++ app/src/main/java/gokhaton/com/no_more_blanket_kick/Fragment/EmergencyCallFragment.java
@@ -0,0 +1,25 @@
+package gokhaton.com.no_more_blanket_kick.Fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import gokhaton.com.no_more_blanket_kick.R;
+
+/**
+ * Created by Earl-PC on 2017. 8. 5..
+ */
+
+public class EmergencyCallFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.emergency_call_fragment,
+                container, false);
+
+        return rootView;
+    }
+}
--- app/src/main/java/gokhaton/com/no_more_blanket_kick/MainActivity.java
+++ app/src/main/java/gokhaton/com/no_more_blanket_kick/MainActivity.java
@@ -1,13 +1,94 @@
 package gokhaton.com.no_more_blanket_kick;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
+
+import gokhaton.com.no_more_blanket_kick.Fragment.BlockMessangerFragment;
+import gokhaton.com.no_more_blanket_kick.Fragment.BlockedFragment;
+import gokhaton.com.no_more_blanket_kick.Fragment.EmergencyCallFragment;
+import gokhaton.com.no_more_blanket_kick.Fragment.MapFragment;
+import gokhaton.com.no_more_blanket_kick.Fragment.TransportFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout main_tab;
+    private FrameLayout container;
+
+    private Fragment fragment1;
+    private Fragment fragment2;
+    private Fragment fragment3;
+    private Fragment fragment4;
+    private Fragment fragment5;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        main_tab = (TabLayout) findViewById(R.id.main_tab);
+        container = (FrameLayout) findViewById(R.id.main_container);
+
+        fragment1 = new EmergencyCallFragment();
+        fragment2 = new BlockedFragment();
+        fragment3 = new MapFragment();
+        fragment4 = new TransportFragment();
+        fragment5 = new BlockMessangerFragment();
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.main_container, fragment1).commit();
+
+        main_tab.addTab(main_tab.newTab().setText("비상 연락"));
+        main_tab.addTab(main_tab.newTab().setText("차단"));
+        main_tab.addTab(main_tab.newTab().setText("위치 등록"));
+        main_tab.addTab(main_tab.newTab().setText("막차 계산"));
+        main_tab.addTab(main_tab.newTab().setText("앱 차단"));
+
+        main_tab.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+                int position = tab.getPosition();
+
+                Fragment selected;
+
+                switch (position){
+                    case 0:
+                        selected = fragment1;
+                        break;
+                    case 1:
+                        selected = fragment2;
+                        break;
+                    case 2:
+                        selected = fragment3;
+                        break;
+                    case 3:
+                        selected = fragment4;
+                        break;
+                    default:
+                        selected = fragment5;
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.main_container, selected).commit();
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
     }
 }
