bd64a2ad566a0725a9f692c8e81ebea333936c51
조승환 <shj95@korea.ac.kr>
2017-08-05 17:05:56 +0900

    LocationRegisterActivity에 back버튼 추가

--- app/src/main/java/gokhaton/com/no_more_blanket_kick/LocationRegisterActivity.java
+++ app/src/main/java/gokhaton/com/no_more_blanket_kick/LocationRegisterActivity.java
@@ -2,6 +2,7 @@ package gokhaton.com.no_more_blanket_kick;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 /**
  * Created by josh on 2017. 8. 5..
@@ -14,6 +15,18 @@ public class LocationRegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_location);
 
-        
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
