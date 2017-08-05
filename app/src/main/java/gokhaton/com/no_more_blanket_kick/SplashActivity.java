4a11d08ae20ed8ae88d0ddc275573bb512ad73ad
earl31 <hanulhalim31@korea.ac.kr>
2017-08-06 02:56:25 +0900

    added permission

--- app/src/main/java/gokhaton/com/no_more_blanket_kick/SplashActivity.java
+++ app/src/main/java/gokhaton/com/no_more_blanket_kick/SplashActivity.java
@@ -1,11 +1,21 @@
 package gokhaton.com.no_more_blanket_kick;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Handler;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private final static int PERMISSIONS_REQ_NUM = 100;
 
     private final int SPLASH_DISPLAY_LENGTH = 1000;
 
@@ -14,14 +24,77 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        new Handler().postDelayed(new Runnable(){
-            @Override
-            public void run() {
-                /* 메뉴액티비티를 실행하고 로딩화면을 죽인다.*/
-                Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
-                SplashActivity.this.startActivity(mainIntent);
-                SplashActivity.this.finish();
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQ_NUM);
+        }
+        else{
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if(!Settings.canDrawOverlays(this)) {
+                    Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                            Uri.parse("package:" + getPackageName()));
+                    startActivityForResult(intent, PERMISSIONS_REQ_NUM);
+                }
+                else{
+                    new Handler().postDelayed(new Runnable(){
+                        @Override
+                        public void run() {
+                        /* 메뉴액티비티를 실행하고 로딩화면을 죽인다.*/
+                            Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
+                            SplashActivity.this.startActivity(mainIntent);
+                            SplashActivity.this.finish();
+                        }
+                    }, SPLASH_DISPLAY_LENGTH);
+                }
             }
-        }, SPLASH_DISPLAY_LENGTH);
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case PERMISSIONS_REQ_NUM:
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+                    if (!Settings.canDrawOverlays(this)) {
+                        //권한 승인이 이루어 지지 않음
+                        finish();
+                    }
+                    else{
+                        new Handler().postDelayed(new Runnable(){
+                            @Override
+                            public void run() {
+                        /* 메뉴액티비티를 실행하고 로딩화면을 죽인다.*/
+                                Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
+                                SplashActivity.this.startActivity(mainIntent);
+                                SplashActivity.this.finish();
+                            }
+                        }, SPLASH_DISPLAY_LENGTH);
+                    }
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQ_NUM:
+
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                            Uri.parse("package:" + getPackageName()));
+                    startActivityForResult(intent, PERMISSIONS_REQ_NUM);
+
+                } else {
+                    finish();
+                }
+                break;
+        }
+
     }
 }
